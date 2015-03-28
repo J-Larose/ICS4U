231663ac7024d2ad3a0dc968567674557b406a83
J-Larose <stonejake1205@gmail.com>
2015-03-27 22:06:17 -0400

    inital commit

---
+++ sortingwork/src/sortingwork/SortingWork.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortingwork;
+/**
+ *
+ * @author Jacob Larose
+ */
+public class SortingWork {
+    
+    public static void main(String args[]) throws Exception
+    {
+       int listLenth;
+       listLenth = (int) ((Math.random()*20)+20);
+       int[] list = new int[listLenth];
+       
+       for(int i=0; i<listLenth; i++){
+           int j= (int) (Math.floor(Math.random()*101));
+           list[i]=j;
+       }
+       
+       for (int x : list){
+           System.out.print (x+" ");
+       }
+       
+       System.out.println();
+       mergeSort(list);
+       System.out.println("merge sorted");
+       
+       for (int x : list){
+           System.out.print (x+" ");
+       }
+       
+       
+       
+    }
+
+    public static int[] mergeSort(int [] list) {
+        if (list.length <= 1) {
+            return list;
+        }
+        
+        // Split the array in half
+        int[] first = new int[list.length / 2];
+        int[] second = new int[list.length - first.length];
+        System.arraycopy(list, 0, first, 0, first.length);
+        System.arraycopy(list, first.length, second, 0, second.length);
+        
+        // Sort each half
+        mergeSort(first);
+        mergeSort(second);
+        
+        // Merge the halves together, overwriting the original array
+        merge(first, second, list);
+        return list;
+    }
+    
+    private static void merge(int[] first, int[] second, int [] result) {
+        // Merge both halves into the result array
+        // Next element to consider in the first array
+        int iFirst = 0;
+        // Next element to consider in the second array
+        int iSecond = 0;
+        
+        // Next open position in the result
+        int j = 0;
+        // As long as neither iFirst nor iSecond is past the end, move the
+        // smaller element into the result.
+        while (iFirst < first.length && iSecond < second.length) {
+            if (first[iFirst] < second[iSecond]) {
+                result[j] = first[iFirst];
+                iFirst++;
+                } else {
+                result[j] = second[iSecond];
+                iSecond++;
+            }
+            j++;
+        }
+        // copy what's left
+        System.arraycopy(first, iFirst, result, j, first.length - iFirst);
+        System.arraycopy(second, iSecond, result, j, second.length - iSecond);
+    }
+    
+   
+}
