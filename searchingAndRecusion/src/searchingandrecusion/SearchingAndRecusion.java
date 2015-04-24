ff4749add3b2d1dd19ab45ad80de8eec12e1ecf1
Jacob Larose <stonejake1205@gmail.com>
2015-04-24 10:54:42 -0400

    searching and recursion

--- searchingAndRecusion/src/searchingandrecusion/SearchingAndRecusion.java
+++ searchingAndRecusion/src/searchingandrecusion/SearchingAndRecusion.java
@@ -5,6 +5,8 @@
  */
 package searchingandrecusion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jacob Larose
@@ -15,7 +17,52 @@ public class SearchingAndRecusion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        boolean tryAgain=false;
+        String list[]={"blue", "Green", "Yellow"};
+        String input;
+        boolean found=false;
+        Scanner scanner= new Scanner(System.in);
         
+        do{
+            input = scanner.nextLine();
+            for(int i=0; i<list.length; i++){
+                if (input.equals(list[i])){
+                    found =true;
+                    System.out.println("found at "+ i);
+                    break;
+                }
+            }
+            if (found==false){
+                System.out.println("Not found");
+            }
+            System.out.println("try again?");
+            input = scanner.nextLine();
+            
+            if ("yes".equals(input)){
+                tryAgain=true;
+                found=false;
+            }else{
+                tryAgain=false;
+            }
+            
+        }while(tryAgain==true);
     }
     
+    public static void sortStringBubble(String list[]) {
+        int j;
+        boolean flag = true;
+        String temp;
+
+        while (flag) {
+            flag = false;
+            for (j = 0; j < list.length - 1; j++) {
+                if (list[j].compareToIgnoreCase(list[j + 1]) > 0) {
+                    temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+    } 
 }
